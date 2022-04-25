36b104149b788cac4a38903ce19e2f4505ff0710
Romain <duclercq.romain@gmail.com>
2022-04-25 16:56:29 +0200

    test repoDiplome

--- src/main/java/projetFormation/repository/IDiplomeRepository.java
+++ src/main/java/projetFormation/repository/IDiplomeRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import projetFormation.model.Diplome;
 
-public interface IDiplomeRepository extends JpaRepository<Diplome, Long>{
+public interface IDiplomeRepository extends Repository<Diplome, Long>{
 
 }
